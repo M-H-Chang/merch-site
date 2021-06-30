e2d7ae0056896512870fafc3ec39c80c287345b2
Min Chang <minchangmhc@gmail.com>
2021-06-30 15:40:00 -0700

    💥 WIP: adding a feature for item details.
    Co-authored-by: Thomas Russell <t.p.russell19@gmail.com>

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,20 +1,50 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Item(props){
-  return (
-    <React.Fragment>
-      <h3>{props.names}</h3>
-      <p>{props.description}</p>
-      <hr/>
-    </React.Fragment>
-  )
+class Item extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      detailVisibleOnPage: false,
+      masterItemList: []
+    };
+  }
+
+  handleClick = () => {
+    this.setState(prevState => ({
+      detailVisibleOnPage: !prevState.detailVisibleOnPage
+    }));
+  }
+
+  
+
+
+    render() {
+      let currentlyVisibleState = null;
+      if (this.state.detailVisibleOnPage) {
+        currentlyVisibleState = <ItemDetail itemDetail={this.state.}
+      }
+      return (
+        <React.Fragment>
+          <h3>{props.names}</h3>
+          <p>{props.description}</p>
+          <hr/>
+        </React.Fragment>
+      )
+    }
+
+  
+  
+  Item.propType = {
+    names: PropTypes.string.isRequired,
+    description: PropTypes.string,
+    quantity: PropTypes.string.isRequired 
+  }
+
+
+
+
 }
 
-Item.propType = {
-  names: PropTypes.string.isRequired,
-  description: PropTypes.string,
-  quantity: PropTypes.string.isRequired 
-}
 
 export default Item;
